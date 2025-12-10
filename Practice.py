06ab20df0d9d77ec6e58c916f7413ac0fac10834
Michael Parnell (MickRocc) <123711382+MickRocc@users.noreply.github.com>
2025-12-10 09:49:43 -0600

    Update description in Practice.py for clarity

--- Practice.py
+++ Practice.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Practice.py
-# A simple practice script demonstrating basic Python features
+# A simple practice script for practiving Python coding skills.
